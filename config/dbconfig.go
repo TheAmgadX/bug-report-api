0103afa0aa62f3421cba07e0a39c20ab8436e86f
TheAmgadX <theamgadx.com>
2025-05-06 14:15:15 +0300

    adding .env file and modifying the dbconfig.go file and adding encryption/decryption methods and valid_email function

--- config/dbconfig.go
+++ config/dbconfig.go
@@ -1,32 +1,51 @@
 package config
 
 import (
-	"gorm.io/gorm"
-	"gorm.io/driver/postgres"
 	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-const (
-	host      = "localhost"
-	user      = "aj_e"
-	password  = "eng464999"
-	dbname    = "bugs_reports"
-	port      = "5432"
-	sslmode   = "disable"
-	TiemeZone = "Africa/Cairo"
-)
+type dbConfig struct {
+	host     string
+	user     string
+	password string
+	dbname   string
+	port     string
+	sslmode  string
+	timeZone string
+}
+
+func initDBConfig() *dbConfig {
+	_ = godotenv.Load(".env")
+
+	return &dbConfig{
+		host:     os.Getenv("DB_HOST"),
+		user:     os.Getenv("DB_USER"),
+		password: os.Getenv("DB_PASSWORD"),
+		dbname:   os.Getenv("DB_NAME"),
+		port:     os.Getenv("DB_PORT"),
+		sslmode:  os.Getenv("DB_SSLMODE"),
+		timeZone: os.Getenv("APP_TIMEZONE"),
+	}
+}
 
 func ConnectDB() (*gorm.DB, error) {
+	cnf := initDBConfig()
 	connStr := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
-		host, user, password, dbname, port, sslmode, TiemeZone)
+		cnf.host, cnf.user, cnf.password, cnf.dbname, cnf.port, cnf.sslmode, cnf.timeZone)
 
 	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{
-		QueryFields:     true,
+		QueryFields: true,
 	})
-	
+
 	if err != nil {
 		return nil, err
 	}
 
-	return db, nil	
+	return db, nil
 }
+
